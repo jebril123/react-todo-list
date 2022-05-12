626b6423cf1b0669cbf70f6e165f2a288618d413
jebril123 <jebrilcadorniga@gmail.com>
2022-05-12 22:59:40 +0800

    weak kaau ka

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-// import App from '../App';
-// import todoinput from '../App';
+import App from '../App';
+import todoinput from '../App';
 
 export default class TodoInput extends Component {
    render() {
-      return <div>Hellow From todo input</div>;
+      return <div>Hellow From todo inputs</div>;
    }
 }
