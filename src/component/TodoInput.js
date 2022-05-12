c04320246be67af3b5dcada259290acca0a155ee
jebril123 <jebrilcadorniga@gmail.com>
2022-05-12 22:09:44 +0800

    first command

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import App from '../App';
-import todoinput from '../App';
+// import App from '../App';
+// import todoinput from '../App';
 
 export default class TodoInput extends Component {
    render() {
