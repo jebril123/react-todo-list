cf2a1483436f96dd5befde625e4078dfad4da415
jebril123 <jebrilcadorniga@gmail.com>
2022-05-12 21:52:22 +0800

    first commit

---
+++ src/component/TodoInput.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import App from '../App';
+import todoinput from '../App';
+
+export default class TodoInput extends Component {
+   render() {
+      return <div>Hellow From todo input</div>;
+   }
+}
---
+++ src/component/TodoItem.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class TodoItem extends Component {
+   render() {
+      return <div>Hellow From Todo Item</div>;
+   }
+}
